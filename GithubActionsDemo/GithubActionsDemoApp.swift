1133517ff2c2be2804d36a0da82b6b145ff24f19
Rui Aureliano <ruiaureliano@gmail.com>
2025-11-24 16:38:45 +0000

    GithubActionsDemo

---
+++ GithubActionsDemo/GithubActionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsDemoApp.swift
+//  GithubActionsDemo
+//
+//  Created by Rui Aureliano on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
